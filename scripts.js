120464cc9e1baee778d5344eb9d92b70ad720f2f
Alenajandro <marcaledom26@gmail.com>
2022-04-24 15:00:44 -0300

    arreglo del problema

--- scripts.js
+++ scripts.js
@@ -1,36 +1,26 @@
-let alenota1 = (8);
-let alenota2 = (7);
+let alenota1 = 7;
+let alenota2 = 7;
 let alenota3 = parseFloat (prompt("ingrese la nota final de ale"));
 
-let joseynota1 = (7);
-let joseynota2 = (9);
+let joseynota1 = 7;
+let joseynota2 = 10;
 let joseynota3 = parseFloat (prompt("ingrese la nota final de josey "));
 
-let joselynnota1 = (8);
-let joselynnota2 = (10);
+let joselynnota1 = 10;
+let joselynnota2 = 10;
 let joselynnota3 = parseFloat (prompt("ingrese la nota final de joselyn "));
 
 
-function calcularPromedio ( alenota1, alenota2, alenota3) {
-const promedio = ( alenota1, alenota2, alenota3) /3;
-return promedio;
-}
-
-function calcularPromedio ( joselynnota1, joselynnota2, joselynnota3 ) {
-const promedio = ( joselynnota1, joselynnota2, joselynnota3 ) /3;
-return promedio;
-}
-
-function calcularPromedio ( joseynota1, joseynota2, joseynota3 ) {
-const promedio = ( joseynota1, joseynota2, joseynota3 ) /3;
-return promedio;
+function calcularPromedio ( nota1, nota2, nota3) {
+    const promedio = ( nota1 + nota2 + nota3) /3;
+    return promedio;
 }
 
 function isAlumnoAprobado (promedio) {
-if( promedio > 6){
-    return "aprobado";
+    if( promedio > 6){
+    return  " aprobado ";
 }
-return "desaprobado";
+    return " desaprobado ";
 }
 
 const promedioAle = calcularPromedio ( alenota1, alenota2, alenota3);
@@ -38,6 +28,6 @@ const promediojoselyn = calcularPromedio ( joselynnota1, joselynnota2, joselynno
 const promediojosey = calcularPromedio ( joseynota1, joseynota2, joseynota3 );
 
 
-alert (" ale esta" + isAlumnoAprobado (promedioAle) + "con nota" + promedioAle);
-alert (" joselyn esta" + isAlumnoAprobado (promediojoselyn) + "con nota" + promediojoselyn);
-alert (" josey esta" + isAlumnoAprobado (promediojosey) + "con nota" + promediojosey);
+alert (" ale esta" + " " + isAlumnoAprobado (promedioAle)+" " + "con nota" + " " + promedioAle);
+alert (" joselyn esta"+ " " + isAlumnoAprobado (promediojoselyn) + " " + "con nota" + " " + promediojoselyn);
+alert (" josey esta" + " " + isAlumnoAprobado (promediojosey) + " " + "con nota" + " " + promediojosey);
