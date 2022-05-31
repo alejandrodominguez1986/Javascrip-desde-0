a1d0fa3dcaf1350d45c06cab9af02850350f6aac
Alenajandro <marcaledom26@gmail.com>
2022-05-30 22:59:51 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -1,187 +1,223 @@
-class Producto {
-  constructor(id, tipo, nombre, marca, utilidad, precio) {
-    this.id = id;
-    this.tipo = tipo;
-    this.marca = marca;
-    this.nombre = nombre;
-    this.utilidad = utilidad;
-    this.precio = precio;
-  }
-}
+const btnMiCarrito = document.querySelector("#btnMiCarrito");
+const CarritoCompras = document.querySelector(".CarritoCompras");
 
-const productosDisponibles = [
-  new Producto(0, "Cuchillo", "Sierra", "Boker", "Panaderia", 1000),
-  new Producto(1, "Cuchillo", "Deshuesador", "Arbolito", "Carnes", 1500),
-  new Producto(2, "Cuchillo", "Santoku", "Boker", "Pescados", 1900),
-  new Producto(3, "Cuchillo", "Nakiri", "Arbolito", "Verduras", 2000),
-  new Producto(4, "Cuchillo", "Fileteador", "Universal", "Filetear", 1500),
-  new Producto(5, "Cuchillo", "Oficio", "Boker", "Oficio", 100),
-  new Producto(6, "Cuchillo", "Abreostras", "Universal", "Ostras", 500),
-  new Producto(7, "Utensillo", "Espatula", "Arbolito", "Pasteleria", 1000),
-  new Producto(8, "Utensillo", "Miserable", "Boker", "Pasteleria", 900),
-  new Producto(9, "Utensillo", "Batidor", "Boker", "Pasteleria", 1000),
-  new Producto(10, "Utensillo", "Cornet", "Universal", "Panaderia", 1500),
-];
-
-let carritoCompra = [];
-
-let inputTextoUtensilio = document.getElementById("inputDeUtensilio");
-let almacenDeProductosDiv = document.getElementById("almacenDeProductos");
-let offcanvasRight = document.getElementById("offcanvasRight");
-
-
-const mostrarProducutos = () => {
-  almacenDeProductosDiv.innerHTML = "";
-  productosDisponibles.forEach((producto) => {
-    almacenDeProductosDiv.innerHTML += `
-            <div id="${producto.id}" class="card " style="width: 18rem;">
-              <div class="card-body ">
-                  <h2 class="card-title"> ${producto.nombre}</h2>
-                    <h5 class="card-title">${producto.marca} </h5>
-                    <p class="card-text">${producto.utilidad}</p>
-                    <p class="card-text">$${producto.precio}</p>
-                    <p class="card-text">Codigo: ${producto.id}</p>
-                    <button onclick= "agregarACarrito(${producto.id})" type="button" id="botonAgregar${producto.id}" >Agregar al carrito </button>
-                    <button onclick= "removerItemCarrito(${producto.id})" type="button" id="botonEliminar${producto.id}" >Eliminas del carrito </button>
-                </div> 
-            </div>
-              `;
-  });
-  
-};
-
-inputTextoUtensilio.addEventListener("change", () => {
-  let buscar = inputTextoUtensilio.value.toLowerCase();
-  let prodABuscar = productosDisponibles.filter((producto) =>
-    producto.tipo.includes(buscar)
-  );
-  almacenDeProductosDiv.innerHTML = "";
-  console.log(prodABuscar);
-  prodABuscar.forEach((producto) => {
-    almacenDeProductosDiv.innerHTML += `
-    <div id="${producto.id}"  class="card "  style="width: 18rem;">
-       <div class="card-body ">
-          <h2 class="card-title">${producto.nombre}</h2>
-             <h5 class="card-title">${producto.marca}</h5>
-             <p class="card-text">${producto.utilidad}</p>
-             <p class="card-text">$${producto.precio}</p>
-             <p id="text" class="card-text">Codigo: ${producto.id}</p>
-             <button onclick= "agregarACarrito(${producto.id})" type="button" id="botonAgregar${producto.id}" >Agregar al carrito </button>
-             <button onclick= "removerItemCarrito(${producto.id})" type="button" id="botonEliminar${producto.id}" >Eliminas del carrito </button>
-        </div> 
-    </div>
-    `;
-  });
-  
+btnMiCarrito.addEventListener("click", () => {
+  CarritoCompras.classList.toggle("active");
 });
- 
-function agregarACarrito(productoID) {
-  let existeProductoEnCarrito = carritoCompra.some(
-    (elemento) => elemento.producto.id == productoID
-  );
-  if (existeProductoEnCarrito) {
-    let indiceProductoCarrito = carritoCompra.findIndex(
-      (elemento) => elemento.producto.id == productoID
-    );
-    //console.log("INDEX CARRITO COMPRA: " +  indiceProductoCarrito)
-    carritoCompra[indiceProductoCarrito].cant++;
-    //console.log("CANTIDAD ACTUALZIADA: " +  carritoCompra[indiceProductoCarrito].cant )
-  } else {
-    let indiceProducto = productosDisponibles.findIndex(
-      (producto) => producto.id == productoID
-    );
-    let productoCarrito = {
-      producto: productosDisponibles[indiceProducto],
-      cant: 1,
-    };
-    carritoCompra.push(productoCarrito);
-    // console.log(productoCarrito)
-    //console.log(carritoCompra)
-  }
-
-  let carritoCompraJson = JSON.stringify(carritoCompra);
-  localStorage.setItem("agregarACarrito", carritoCompraJson);
-  offcanvas();
-  Swal.fire({
-    position: 'center-center',
-    icon: 'success',
-    title: 'Agregastes este producto',
-    showConfirmButton: false,
-    timer: 1700
-  })
-  
-}
-
-function removerItemCarrito(productoID) {
-  let eliminarProductoEnCarrito = carritoCompra.some(
-    (elemento) => elemento.producto.id == productoID
-  );
-  if (eliminarProductoEnCarrito) {
-    let indiceProductoEliminadoCarrito = carritoCompra.findIndex(
-      (elemento) => elemento.producto.id == productoID
-    );
-    //console.log("INDEX CARRITO COMPRA: " +  indiceProductoEliminadoCarrito)
-    carritoCompra[indiceProductoEliminadoCarrito].cant--;
-    //console.log("CANTIDAD ACTUALZIADA: " +  carritoCompra[indiceProductoEliminadoCarrito].cant )
-  } else {
-    let indiceProductoEliminado = productosDisponibles.findIndex(
-      (producto) => producto.id == productoID
-    );
-    let productoCarrito = {
-      producto: productosDisponibles[indiceProductoEliminado],
-      cant: 0,
-    };
-
-    carritoCompra.push(productoCarrito);
-    console.log(productoCarrito);
-    console.log(carritoCompra);
-  }
-
-  let carritoCompraJson = JSON.stringify(carritoCompra);
-  localStorage.setItem("removerItemCarrito", carritoCompraJson);
-  offcanvas();
- 
-  let timerInterval
-Swal.fire({
-  title: 'Lo eliminastes',
-  icon: 'question',
-  html: 'ESTE MENSAJE SE ELIMINARA EN  <b></b>.',
-  timer: 2000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
+//declaramos por medio de una variable nuestro productos para luego ser utilizados
+let productos = [
+  {
+    id: 1,
+    nombre: "Sierra",
+    precio: 1000,
+    marca:  "Boker",
+    stock: 10 ,
+    uso: "Panaderia",
+    imagen: "./images/Sierra.jpg",
   },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer')
-  }
-})
+  {
+    id: 2,
+    nombre: "Deshuesador",
+    precio: 1500,
+    marca:  "Boker",
+    stock: 10 ,
+    uso: "Carniceria",
+    imagen: "./images/Deshuesador.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Santoku",
+    precio: 1900,
+    marca:  "Boker",
+    stock: 10 ,
+    uso: "Fileteado de pescados",
+    imagen: "./images/Santoku.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Nakiri",
+    precio: 2000,
+    marca:  "Arbolito",
+    stock: 10 ,
+    uso: "Corte de verduras",
+    imagen: "./images/Nakiri.jfif",
+  },
+  {
+    id: 5,
+    nombre: "Oficio",
+    precio: 500,
+    marca:  "Boker",
+    stock: 10 ,
+    uso: "Cuchillo para pelear o tornear",
+    imagen: "./images/Oficio.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Abreostras",
+    precio: 700,
+    marca:  "3 Claveles",
+    stock: 10 ,
+    uso: "Para abrir ostras de forma segura",
+    imagen: "./images/Abreostras.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Espatula de codo",
+    precio: 800,
+    marca:  "3 Claveles",
+    stock: 10 ,
+    uso: "Para la pasteleria",
+    imagen: "./images/Espatula de codo.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Miserable",
+    precio: 1200,
+    marca:  "Hudson",
+    stock: 10 ,
+    uso: "Espatula de silicona para la pasteleria",
+    imagen: "./images/Miserable.jfif",
+  },
+  {
+    id: 9,
+    nombre: "Batidor",
+    precio: 850,
+    marca:  "Hudson",
+    stock: 10 ,
+    uso: "Para mesclar preparaciones",
+    imagen: "./images/Batidor.jpg",
+  },
+  {
+    id: 10,
+    nombre: "Cornet metalico",
+    precio: 730,
+    marca:  "universal",
+    stock: 10 ,
+    uso: "Para la panaderia",
+    imagen: "./images/Cornet metalico.jpg",
+  },
+];
+const caja = document.querySelector(".caja");
+
+let carrito = [];
+
+function mostrarProducutos() {
+  const cardsMap = productos.map((element) => {
+    const { imagen, nombre,uso, precio, id, stock } = element;
+    if (stock >= 1) {
+      return `<div class="cajita">
+      <div class="divImagen">
+        <img class="imgDiv" src="${imagen}">
+      </div>
+      <div>
+        <h3>${nombre}</h3>
+        <p>$${uso}</p>
+        <p>$${precio}</p>
+        <p>Stock: ${stock}</p>
+        <button id="agregar${id}">Agregar</button>
+      </div>
+    </div>`;
+    } else {
+      return `<div class="cajita">
+      <div class="divImagen">
+        <img class="imgDiv" src="${imagen}">
+      </div>
+      <div>
+         h3>${nombre}</h3>
+        <p>$${uso}</p>
+        <p>$${precio}</p>
+        <p>Stock: ${stock}</p>
+        <button disabled="true" id="agregar${id}">Agregar</button>
+      </div>
+    </div>`;
+    }
+  });
+
+  caja.innerHTML = cardsMap.join("");
+
+  escucharBotonAgregar();
 }
 
-function offcanvas() {
-  offcanvasRight.innerHTML = "";
-  productosDisponibles.forEach((producto) => {
-    offcanvasRight.innerHTML += `
-            <div id="${producto.id}" class="card " style="width: 18rem;">
-              <div class="card-body ">
-                  <h2 class="card-title"> ${producto.nombre}</h2>
-                    <h5 class="card-title">${producto.marca} </h5>
-                    <p class="card-text">${producto.utilidad}</p>
-                    <p class="card-text">$${producto.precio}</p>
-                    <p class="card-text">Codigo: ${producto.id}</p>
-                    <button onclick= "agregarACarrito(${producto.id})" type="button" id="botonAgregar${producto.id}" >Agregar al carrito </button>
-                    <button onclick= "removerItemCarrito(${producto.id})" type="button" id="botonEliminar${producto.id}" >Eliminas del carrito </button>
-                </div> 
-            </div>
-              `;
+function escucharBotonAgregar() {
+  productos.forEach((producto, index) => {
+    document
+      .querySelector(`#agregar${producto.id}`)
+      .addEventListener("click", () => {
+        enviarAlCarrito(producto);
+      });
+  });
+}
+/*Con esta funcion por medio de los metodos SOME (que verifica la existencia de nuestro producto en el carrito) para luego utilizar el metodo map (para incorporar nuestro producto dentro del carrito )*/
+function enviarAlCarrito(producto) {
+  const existe = carrito.some((element) => element.id === producto.id);
+
+  productos.map((element) => {
+    if (element.id === producto.id) {
+      element.stock--;
+      return element;
+    }
+  });
+
+  const productoAlCarrito = { ...producto, cantidad: 1 };
+  /*Eliminamos el stock ya que dentro del carrito necesitamos solo la cantidad del producto elegido */
+  delete productoAlCarrito.stock;
+
+  if (existe) {
+    carrito.map((element) => {
+      if (element.id === producto.id) {
+        element.cantidad++;
+        return element;
+      }
+    });
+  } else {
+    carrito.push(productoAlCarrito);
+  }
+  //llamamos a las funciones para mostrar lo seleccionado previamente//
+  mostrarProducutos();
+  pintarCarrito();
+}
+
+//de esta manerta con un innerHTML creamos cada card para mostrar todos nuestros producctos //
+function pintarCarrito() {
+  CarritoCompras.innerHTML = "";
+  carrito.forEach((element) => {
+    CarritoCompras.innerHTML += `<div class="cajita">
+        <div class="divImagen" >
+          <img class="imgCarritoCompras" src="${element.imagen}">
+        </div>
+        <div>
+          <h3>${element.nombre}</h3>
+          <p>$${element.uso}</p>
+          <p>$${element.precio}</p>
+          <p>Cant.: ${element.cantidad}</p>
+          <button class="btnBorrar" id="${element.id}">Eliminar</button>
+          </div>
+      </div>`;
+  });
+  borrarProducto();
+}
+
+function borrarProducto() {
+  let btnBorrar = document.querySelectorAll(".btnBorrar");
+  btnBorrar.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      // selecciono el id del boton //
+      let id = parseInt(e.target.id);
+
+      //Necesito verificar la cantidad antes de borrar, la guardo en la variable cantidad.//
+      let prod = carrito.find((element) => element.id === id);
+      let cantidad = prod.cantidad;
+  
+      carrito = carrito.filter((element) => element.id !== id);
+
+      // Buscamos el producto, y en su propiedad stock  la cambiamos por  el valor de la variable cantidad //
+      let prodCarrito = productos.find((element) => element.id === id);
+      prodCarrito.stock += cantidad;
+
+      // llamamos alas funciones para mostrar lo seleccionadpo previamente  //
+      pintarCarrito();
+      mostrarProducutos();
+    });
   });
 }
 
